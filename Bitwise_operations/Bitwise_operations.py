beee91ae98cbfdb32a14f30780e9a89fb48c8452
Sameony <iam2001sam@gmail.com>
2021-05-30 14:48:03 +0530

    Bitwise Operations I

---
+++ Bitwise_operations/Bitwise_operations.py
@@ -0,0 +1,17 @@
+#Bitwise operators and their syntax
+#1. Bitwise And (&): x&y
+#2. Bitwise Or (|): x|y
+#3. Bitwise Not (~): ~x
+#4. Bitwise XOR (^): x^y
+#5. Bitwise Left Shift (<<): x<</x<<n
+#6. Bitwise Right Shift (>>): x>>/x>>
+
+a,b,c=3,4,5
+print("a= ",a)
+print("b= ",b)
+print("And: a&b", a&b)
+print("Or: a|b",a|b)
+print("NOT: ~a", ~a)
+print("XOR: a^b",a^b)
+print("Left shift: a<<",a<<1)
+print("Right shift: a>>",a>>1)
