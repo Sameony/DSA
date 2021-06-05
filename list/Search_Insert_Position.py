d62acdb71bd367db683a940e8738bf6e5167b48c
Sameony <iam2001sam@gmail.com>
2021-06-05 18:32:19 +0530

    Search insert position

---
+++ list/Search_Insert_Position.py
@@ -0,0 +1,7 @@
+def searchInsert(self, nums: List[int], target: int) -> int:
+    try:
+        return nums.index(target)
+    except ValueError:
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
